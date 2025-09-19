776ad0dc2d76d2a7a9bb094892bc0cd8e2ab52ee
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-09-19 04:20:32 -0600

    Created database tables

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,8 +1,88 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL PRIMARY KEY COMMENT 'primary key',
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name VARCHAR(255) COMMENT 'User Name',
-  email VARCHAR(255) UNIQUE COMMENT 'User Email',
-  picture VARCHAR(255) COMMENT 'User Picture'
+CREATE TABLE IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL PRIMARY KEY COMMENT 'primary key',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name VARCHAR(255) COMMENT 'User Name',
+    email VARCHAR(255) UNIQUE COMMENT 'User Email',
+    picture VARCHAR(255) COMMENT 'User Picture'
 ) default charset utf8mb4 COMMENT '';
+
+-- CREATE TABLES BEGIN
+CREATE TABLE allspice_recipes (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    title VARCHAR(255) NOT NULL,
+    instructions VARCHAR(5000) NOT NULL,
+    img VARCHAR(2000) NOT NULL,
+    category ENUM(
+        'breakfast',
+        'lunch',
+        'dinner',
+        'snack',
+        'dessert'
+    ),
+    creator_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES accounts (id) ON DELETE CASCADE
+)
+
+CREATE TABLE allspice_altered_recipes (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    recipe_id INT NOT NULL,
+    FOREIGN KEY (recipe_id) REFERENCES allspice_recipes (id) ON DELETE CASCADE
+)
+
+CREATE TABLE allspice_ingredients (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    title VARCHAR(255) NOT NULL,
+    quantity VARCHAR(255) NOT NULL,
+    recipe_id INT NOT NULL,
+    creator_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (recipe_id) REFERENCES allspice_recipes (id) ON DELETE CASCADE,
+    FOREIGN KEY (creator_id) REFERENCES accounts (id) ON DELETE CASCADE
+)
+
+CREATE TABLE allspice_favorites (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    account_id VARCHAR(255) NOT NULL,
+    recipe_id INT NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES accounts (id) ON DELETE CASCADE,
+    FOREIGN KEY (recipe_id) REFERENCES allspice_recipes (id) ON DELETE CASCADE
+)
+
+CREATE TABLE allspice_recipe_notes (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    recipe_id INT NOT NULL,
+    note_body VARCHAR(5000) NOT NULL,
+    creator_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (recipe_id) REFERENCES allspice_recipes (id) ON DELETE CASCADE,
+    FOREIGN KEY (creator_id) REFERENCES accounts (id) ON DELETE CASCADE
+)
+
+CREATE TABLE allspice_recipe_comments (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    comment_body VARCHAR(5000) NOT NULL,
+    creator_id VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE allspice_likes (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    comment_id INT NOT NULL,
+    account_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (comment_id) REFERENCES allspice_recipe_comments (id) ON DELETE CASCADE,
+    FOREIGN KEY (account_id) REFERENCES accounts (id) ON DELETE CASCADE
+)
+
+-- CREATE TABLES END
