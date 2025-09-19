7dec0da641650900dc0a22dc075c91196fbc07bc
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-09-19 03:10:45 -0600

    Changed Recipe Name property to Title as it is a keyword in MySQL.

--- server/Models/Recipe.cs
+++ server/Models/Recipe.cs
@@ -4,7 +4,7 @@ namespace allspice.Models;
 
 public class Recipe : RepoItem<int>
 {
-  [MaxLength(255)] public string Name { get; set; }
+  [MaxLength(255)] public string Title { get; set; }
   [MaxLength(5000)] public string Instructions { get; set; }
   [Url, MaxLength(2000)] public string Img { get; set; }
   [MaxLength(255)] public string Category { get; set; }
