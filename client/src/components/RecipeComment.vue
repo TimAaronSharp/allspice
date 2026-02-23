da7a522d7d0ae72231f1db5501911cbd76531af9
TimAaronSharp <timothyaaron.sharp@gmail.com>
2026-02-23 10:08:09 -0700

    CREATED: EditRecipeCommentForm.vue
    REFACTORED: RecipeComment.vue - Added computed for account, edit comment button (mdi-pencil), and v-if to only show those buttons if user created comments.

--- client/src/components/RecipeComment.vue
+++ client/src/components/RecipeComment.vue
@@ -1,9 +1,12 @@
 <script setup>
+import { AppState } from '@/AppState.js';
 import { RecipeComment } from '@/models/RecipeComment.js';
 import { recipeCommentsService } from '@/services/RecipeCommentsService.js';
 import { logger } from '@/utils/Logger.js';
 import { Pop } from '@/utils/Pop.js';
+import { computed } from 'vue';
 
+const account = computed(() => AppState.account)
 
 const props = defineProps({
   recipeCommentProp: { type: RecipeComment, required: true }
@@ -27,8 +30,9 @@ async function deleteRecipeComment() {
   <div class="d-flex">
     <img :src="recipeCommentProp.creator.picture" :alt="recipeCommentProp.creator.name + `'s profile picture.'`">
     <div class="d-flex flex-column">
-      <div>
+      <div v-if="account?.id == recipeCommentProp.creatorId">
         <button @click="deleteRecipeComment()" class="mdi mdi-close-circle text-red transparent-btn-style"></button>
+        <button class="mdi mdi-pencil transparent-btn-style"></button>
       </div>
       <p>{{ recipeCommentProp.creator.name }}</p>
       <p>{{ recipeCommentProp.body }}</p>
